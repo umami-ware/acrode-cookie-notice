a9a81852561c069e8ab60273d4f7d9fd5520dc34
Johannes <47254538+JohannesArtmeier@users.noreply.github.com>
2019-10-14 19:58:20 +0200

    Update cookie-notice.php

--- cookie-notice.php
+++ cookie-notice.php
@@ -3,7 +3,7 @@
 Plugin Name: acrode Cookie Notice
 Plugin URI: https://github.com/JohannesArtmeier/acrode-Cookie-Notice-Plugin
 Description: acrode Cookie Notice allows you to elegantly inform users that your site uses cookies. This plugin also helps website owners to comply with the EU cookie law.
-Version: 2.3.2
+Version: 2.4.0
 Author: acrode
 Author URI: https://acrode.com
 */
